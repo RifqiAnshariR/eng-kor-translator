1453a7727ee8dc2fa9471d1ad3a0888e080032cd
RifqiAnshariR <ansharirifqi2@gmail.com>
2025-03-14 03:09:02 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,17 @@
+# Main
 MODEL_NAME = "LGAI-EXAONE/EXAONE-3.5-2.4B-Instruct"
+
+# Interface
+BASE_FONTSTYLE = "color: #333; font-family: Arial;"
+HEADER_FONTSTYLE = f"font-size: 16px; font-weight: bold; {BASE_FONTSTYLE}"
+LABEL_1_FONTSTYLE = f"font-size: 15px; font-weight: bold; {BASE_FONTSTYLE}"
+LABEL_2_FONTSTYLE = f"font-size: 15px; {BASE_FONTSTYLE}"
+
+BASE_BUTTONSTYLE = "color: white; font-size: 15px; padding: 5px; border-radius: 5px;"
+KOR_ENG_BUTTONSTYLE = f"background-color: green; {BASE_BUTTONSTYLE}"
+ENG_KOR_BUTTONSTYLE = f"background-color: blue; {BASE_BUTTONSTYLE}"
+GRAMMAR_BUTTONSTYLE = f"background-color: blue; {BASE_BUTTONSTYLE}"
+
+# Log
+LOG_DIR = "log"
+LOG_NAME = "app.log"
